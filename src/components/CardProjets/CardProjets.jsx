d93ba6c7c0501703fbd0915897bd0478abc5547d
Marine Cede <marine.cedee@gmail.com>
2025-03-17 16:10:13 +0100

    fix: deploiement et img publique

--- src/components/CardProjets/CardProjets.jsx
+++ src/components/CardProjets/CardProjets.jsx
@@ -2,7 +2,7 @@ import "./CardProjets.scss"
 export default function CardProjets({projet, }){
     return (
         <div className="card">
-            <img src={`../../../public/imgProjets/${projet.id}`} alt={projet.altImg} className="card__image"></img>
+            <img src={`./imgProjets/${projet.id}`} alt={projet.altImg} className="card__image"></img>
             <h2 className="card__title">{projet.name}</h2>
             <p className="card__description">{projet.description}</p>
             <a href={projet.url} className="card__url">Cliquez ici pour le voir sur Github ! </a>
